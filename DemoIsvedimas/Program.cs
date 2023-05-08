a9f40b867d058912e3583b97c8974d21fdf07aea
Martynas <Martynas@DESKTOP-HTUUCFV>
2023-05-08 11:09:00 +0300

    Add project files.

---
+++ DemoIsvedimas/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DemoIsvedimas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // lietuviškų simbolių rodymas
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Labas, pasauli!");
+            Console.WriteLine();
+
+            // Reikšmių / kintamųjų išvedimas
+            Console.Write("Mano ūgis: ");
+            Console.WriteLine(1.91);
+
+            float tecioUgis = 1.81f;
+            Console.Write("Mano tėčio ūgis: ");
+            Console.WriteLine(tecioUgis);
+            Console.WriteLine();
+
+            // Skirtingi kintamųjų + teksto išvedimai
+            int amzius = 15;
+            float ugis = 1.77f;
+
+            Console.WriteLine("Labas, man yra 15 metų ir mano ūgis yra 1.77 metro");
+            Console.WriteLine("Labas, man yra " + amzius + " metų ir mano ūgis yra " + ugis + " metro");
+            Console.WriteLine("Labas, man yra {0} metų ir mano ūgis yra {1} metro", amzius, ugis);
+            Console.WriteLine($"Labas, man yra {amzius} metų ir mano ūgis yra {ugis} metro");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ DemoKintamieji/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace DemoKintamieji
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Komentaras per vieną eilutę
+
+            /*
+            
+                Komentaras
+                per
+                kelias
+                eilutes
+
+             */
+
+            // Sveikieji
+            int skaicius; // be nustatytos reikšmės
+            int skaiciusSuReiksme = 7; // su nustatyta reikšme
+            int skaiciusIsKitoKintamojo = skaiciusSuReiksme; // reikšmė iš kito kintamojo
+
+            // Trupmeniniai
+            float ugis = 1.77f;
+            float ugisSveikasis = 2f;
+            double ugisTikslesnis = 1.77777777777777; // daugiau skaičių po kablelio
+
+            // Simboliai
+            char simbolis = '@'; // suteikiam pradinę reikšmę
+            simbolis = '#';
+            simbolis = '$';
+            char kitasSimbolis = simbolis;
+            string tekstas = "kažkoks tekstas kažkoks tekstas";
+
+            // Loginiai
+            bool arLyja = false;
+            bool arPavasaris = true;
+        }
+    }
+}
