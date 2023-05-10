f010370411e4311b50b16703d5fa43898bc10ad3
MartynasVCS <m.satas@gmail.com>
2023-05-10 11:02:53 +0300

    Add demo code for switch statement

---
+++ DemoSwitch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DemoSwitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lietuviškų simbolių rodymas
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            int amzius = 17;
+
+            switch (amzius)
+            {
+                case 18:
+                    Console.WriteLine("Žmogui yra 18 metų");
+                    break;
+                case 12:
+                    Console.WriteLine("Žmogui prasideda paauglystė");
+                    break;
+                default:
+                    Console.WriteLine("Žmogui nei 18 nei 12");
+                    break;
+            }
+        }
+    }
+}
